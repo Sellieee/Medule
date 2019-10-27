3829744807cf06ee29ae3024b925a688e7b123c4
Sellieee <Sellieee@hotmail.com>
2019-10-27 16:56:26 +1100

    created initial files for react. Removed previous non-react authentication files

---
+++ src/components/sign-up.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+import axios from "axios"
+
+render(){
+   return (
+      <div className="signUpForm">
+         <h4>Sign Up</h4>
+         <form className="form-horizontal">
+            <div className="form-group">
+               <div className="col-1 col-ml-auto">
+                  <label className="form-label" htmlFor="username">Username</label>
+               </div>
+               <div className="col-3 col-mr-auto">
+                  <input className="form-input"
+                     type="text"
+                     id="username"
+                     name="username"
+                     placeholder="Username"
+                     value={this.state.username}
+                     onChange={this.handleChange}
+                  />
+               </div>
+            </div>
+         </form>
+      </div>
+   )
+}
