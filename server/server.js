4a49fe0e21ac6bd31e974f5d2c75d9de51fb25fa
Sellieee <Sellieee@hotmail.com>
2019-11-01 20:58:09 +1100

    added npm packages

---
+++ server/server.js
@@ -0,0 +1,9 @@
+// Npm packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const session = require("express-session");
+const dbConnection = require("./database");
+const MongoStore = require("connect-mongo")(session);
+const passport = require("./passport");
+const app = express();
