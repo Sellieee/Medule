d4bf77f0a6808d648ef776cea8bc204783cbda37
Sellieee <Sellieee@hotmail.com>
2019-11-01 21:09:29 +1100

    added passport, morgan and sessions to the server

--- server/server.js
+++ server/server.js
@@ -7,3 +7,32 @@ const dbConnection = require("./database");
 const MongoStore = require("connect-mongo")(session);
 const passport = require("./passport");
 const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Routes
+const user = require("./routes/user");
+app.use("/user", user);
+
+// Middleware
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// Sessions
+app.use(session({
+   secret: "pikachu", // Needed to pick any word to make the hash secure
+   store: new MongoStore({ mongooseConnection: dbConnection }),
+   resave: false,
+   saveUninitialized: false
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Start the server
+app.listen(PORT, () => {
+   console.log(`App listening on port: ${PORT}`)
+});
