7578c21be295aea1ede6304f87d35920ae808d97
Sellieee <Sellieee@hotmail.com>
2019-11-03 22:12:11 +1100

    added html routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,73 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Signup from "./components/Signup.js"
+import React, { Component } from "react";
+import axios from "axios";
+import { Route, Link } from "react-router-dom";
+import Signup from "./components/Signup";
+import LoginForm from "./components/login";
+import Navbar from "./components/navbar";
+import Home from "./components/home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loggedIn: false,
+      username: null
+    };
+
+    this.getUser = this.getUser.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.updateUser = this.updateUser.bind(this);
+  };
+
+  componentDidMount() {
+    this.getUser();
+  };
+
+  getUser() {
+    axios.get("/user/").then(response => {
+      console.log("Get user response: " + response.data);
+      if (response.data.user) {
+        console.log("Get user: There is a user saved in the server session: " + response.data.user);
+        this.setState({
+          loggedIn: true,
+          username: response.data.user.username
+        });
+      }
+      else {
+        console.log("Get user: no user found");
+        this.setState({
+          loggedIn: false,
+          username: null
+        });
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn} />
+        {this.state.loggedIn && <p>Welcome to Medule, {this.state.username}!</p>}
+
+        {/* Home page */}
+        <Route exact path="/"
+          component={Home} />
+
+        {/* Login page */}
+        <Route path="/login"
+          render={() =>
+            <LoginForm updateUser={this.updateUser} />}
+        />
+
+        {/* Signup page */}
+        <Route path="/signup"
+          render {() =>
+            <Signup />}
+        />
+
+      </div>
+    );
+  }
+};
 
 export default App;
