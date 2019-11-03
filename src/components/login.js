a7315134ea5a0782802808060754e6e9462b8867
Sellieee <Sellieee@hotmail.com>
2019-11-03 17:03:30 +1100

    added html fot login page

--- src/components/login.js
+++ src/components/login.js
@@ -20,4 +20,82 @@ class LoginForm extends Component {
          [event.target.name]: event.target.value
       });
    }
-}
+
+   handleSubmit(event) {
+      event.preventDefault();
+      console.log("handleSubmit");
+
+      axios.post("/user/login", {
+         username: this.state.username,
+         password: this.state.password
+      })
+         .then(response => {
+            console.log("Login response: " + response);
+
+            if (response.status === 200) {
+               this.props.updateUser({
+                  loggedIn: true,
+                  username: response.data.username
+               });
+               this.setState({
+                  RedirectTo: "/"
+               });
+            };
+         })
+         .catch(error => {
+            console.log("Login error: " + error);
+         });
+   };
+
+   render() {
+      if (this.state.RedirectTo) {
+         return <Redirect to={{ pathname: this.state.RedirectTo }} />
+      } else {
+         return (
+            <div>
+               <h4>Login</h4>
+               <form className="form-horizontal">
+                  <div className="form-group">
+                     <div className="col -1 col-ml-auto">
+                        <label className="form-label" htmlFor="username">Username</label>
+                     </div>
+                     <div className="col-3 col-mr-auto">
+                        <input className="form-input"
+                           type="text"
+                           id="username"
+                           name="username"
+                           placeholder="Username"
+                           value={this.state.username}
+                           onChange={this.handleChange}
+                        />
+                     </div>
+                  </div>
+                  <div className="form-group">
+                     <div className="col -1 col-ml-auto">
+                        <label className="form-label" htmlFor="password">Password</label>
+                     </div>
+                     <div className="col-3 col-mr-auto">
+                        <input className="form-input"
+                           type="text"
+                           id="password"
+                           name="password"
+                           placeholder="password"
+                           value={this.state.password}
+                           onChange={this.handleChange}
+                        />
+                     </div>
+                  </div>
+                  <div className="form-group">
+                     <div className="col-7"></div>
+                     <button className="btn btn-primary col-1 col-mr-auto"
+                        onClick={this.handleSubmit}
+                        type="submit">Login</button>
+                  </div>
+               </form>
+            </div>
+         );
+      };
+   };
+};
+
+export default Login;
