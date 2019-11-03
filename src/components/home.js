1bfa61154c4b4225aa2e575563175db19778dc1b
Sellieee <Sellieee@hotmail.com>
2019-11-03 17:47:41 +1100

    added mock home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,1 +1,19 @@
 // No authentication needed
+
+import React, { Component } from "react";
+
+class Home extends Component {
+   constructor() {
+      super()
+   }
+
+   render() {
+      return (
+         <div>
+            <p>Home page</p>
+         </div>
+      )
+   };
+};
+
+export default Home;
