02e9dabba89368b07d2e037d30dd09693a2f2075
Sellieee <Sellieee@hotmail.com>
2019-11-13 20:27:40 +1100

    added client side google places api call

--- src/components/map.js
+++ src/components/map.js
@@ -1,51 +1,56 @@
 import React, { Component } from "react";
-import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
-import axios from "axios";
-
-// const getCleanSearch = search => search.replace(" ", "");
-// const getMapsUrl = search => `https://maps.googleapis.com/maps/api/place/textsearch/json?query=general+practitioners+in+${getCleanSearch(search)}&key=AIzaSyAi5FmO4ICcm5wSgSML69KMj4ebRXObtwY`
-const search = "";
+import SearchBar from "material-ui-search-bar";
+import Script from "react-load-script";
 
 class Map extends Component {
    constructor(props) {
       super(props);
-
-      // axios.get("http://maps.googleapis.com/maps/api/place/textsearch/json?query=general+practitioners+in+" + search + "&key=AIzaSyAi5FmO4ICcm5wSgSML69KMj4ebRXObtwY")
-      //    .then(response => {
       this.state = {
-         search: ""
+         city: "",
+         query: "",
       };
-      //       console.log(response);
-      //    });
+   }
+
+   handleScriptLoad() {
+      var options = { types: [`(cities)`] };
+      this.autocomplete = new google.maps.places.Autocomplete(
+         document.getElementById("autocomplete"),
+         options);
+
+      this.autocomplete.setFields(["address_components",
+         "formatted_address"]);
+
+      this.autocomplete.addListener("place_changed",
+         this.handlePlaceSelect);
    };
 
-   componentDidMount() {
-      axios.get('https://maps.googleapis.com/maps/api/place/details/json?placeid=' +
-         search + '&key=AIzaSyAi5FmO4ICcm5wSgSML69KMj4ebRXObtwY')
-         .then(response => {
-            console.log(response)
-         })
-         .catch(err => {
-            console.log(err)                     //Axios entire error message
-            console.log(err.response) //Google API error message 
-         })
-   }
+   handlePlaceSelect = () => {
+      const addressObject = this.autocomplete.getPlace();
+      const address = addressObject.address_components;
+
+      if (address) {
+         this.setState({
+            city: address[0].long_name,
+            query: addressObject.formatted_address,
+         });
+      };
+   };
+
    render() {
       return (
          <div>
-            {/* <input
-               name="search"
-               placeholder="Search for a Location here!"
-               className="searchbox"
-               onChange={event => this.setState({ search: event.target.value })}
-               value={this.state.search}
+            <Script url="https://maps.googleapis.com/maps/api/place/textsearch/xml?query=general+practitioners+in+Sydney&key=AIzaSyAi5FmO4ICcm5wSgSML69KMj4ebRXObtwY"
+               onLoad={this.handleScriptLoad}
             />
-            <div>
-               <img alt="Map of searched Location" src='https://maps.google.com/maps/contrib/113202928073475129698/photos\"\u003eEmily Zimny\u003c/a\u003e&key=AIzaSyAi5FmO4ICcm5wSgSML69KMj4ebRXObtwY' />
-            </div> */}
+            <SearchBar id="autocomplete" placeholder="Search for a Location Here" hintText="Search Location" value={this.state.query}
+               style={{
+                  margin: "0 auto",
+                  maxWidth: 800,
+               }}
+            />
          </div>
-      )
-   }
+      );
+   };
 };
 
 export default Map;
