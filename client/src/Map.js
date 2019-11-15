b5a51c330e36a77a5d78d5f7411f4b8b2bfa3cfe
Sellieee <Sellieee@hotmail.com>
2019-11-15 12:26:13 +1100

    Added text to the markers, click failing

--- client/src/Map.js
+++ client/src/Map.js
@@ -2,7 +2,6 @@
 import React, { Component } from "react";
 import { GoogleMap, Marker, InfoWindow, withGoogleMap, withScriptjs } from "react-google-maps";
 import item from "./places.json";
-//const fs = require("fs")
 
 const AnyReactComponent = ({ text }) => (
     <div style={{
@@ -24,56 +23,52 @@ class SimpleMapPage extends Component {
     defaultProps = {
         center: { lat: -37.88, lng: 145.018 },
         zoom: 14,
-        infoWindows: this.props.item.map(p => {
-            return { isOpen: false };
-        })
+    };
+
+    state = {
+        item: item,
+        handleToggleOpen(item) {
+            this.setState({
+                position: {
+                    lat: item.latitude,
+                    lng: item.longitude
+                }
+            })
+        },
+    }
+
+    render() {
+        return (
+            <div style={{ width: "100%", height: "400px" }}>
+                <GoogleMap
+                    bootstrapURLKeys={{ key: this.props.apiKey }}
+                    defaultCenter={{ lat: -37.88, lng: 145.018 }}
+                    defaultZoom={14}
+                >
+                    {this.state.item.map((item, i) => {
+                        let lat = parseFloat(item.latitude, 10);
+                        let lng = parseFloat(item.longitude, 10);
+                        // eslint-disable-next-line no-unreachable
+                        return (
+                            <Marker
+                                id={item.id}
+                                key={item.id}
+                                position={{ lat: lat, lng: lng }}
+                                title={item.name}
+                                onClick={() => this.handleToggleOpen(item)}
+                            >
+                                {this.state.position &&
+                                    <InfoWindow position={this.state.position}>
+                                        <h1>{this.props.location.venue.name}</h1>
+                                    </InfoWindow>
+                                }
+                            </Marker>
+                        )
+                    })}
+                </GoogleMap>
+            </div >
+        );
     };
 };
-
-{
-    onToggleOpen: ({ infoWindows }) => selectedIndex => ({
-        infoWindows: infoWindows.map((iw, i) => {
-            iw.isOpen = selectedIndex === i;
-            return iw;
-        })
-    })
-};
-
-state = {
-    item: item
-};
-
-
-render() {
-    return (
-        <div style={{ width: "100%", height: "400px" }}>
-            <GoogleMap
-                bootstrapURLKeys={{ key: this.props.apiKey }}
-                defaultCenter={{ lat: -37.88, lng: 145.018 }}
-                defaultZoom={14}
-            >
-                {this.state.markers.map((item, i) => {
-                    let lat = parseFloat(item.latitude, 10);
-                    let lng = parseFloat(item.longitude, 10);
-                    // eslint-disable-next-line no-unreachable
-                    return (
-                        <Marker
-                            id={item.id}
-                            key={item.id}
-                            position={{ lat: lat, lng: lng }}
-                            title="Click to View"
-                            onClick={props.onToggleOpen.bind(this, i)}
-                        >
-                            <InfoWindow onCloseClick={props.onToggleOpen.bind(i)}>
-                                <div>{item.name}</div>
-                            </InfoWindow>
-                        </Marker>
-                    )
-                })}
-            </GoogleMap>
-        </div >
-    );
-};
-
 
 export default withScriptjs(withGoogleMap(SimpleMapPage))
